7ad01a8168e91563a5a78ec0d4d507a8b11747b4
dooishe <tugievzx@mail.ru>
2025-07-14 15:19:33 +0500

    Add backend-practice file

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -3,5 +3,5 @@ xhr.addEventListener("load", () => {
   console.log(xhr.response);
 });
 
-xhr.open("GET", "https://supersimplebackend.dev/products/first");
+xhr.open("GET", "https://supersimplebackend.dev/images/apple.jpg");
 xhr.send();
